e58aa5b399e674891775adb121f344b21472fad6
Bastian Gruber <gruberbastian@me.com>
2018-06-25 17:30:56 +0200

    Add overview and velocity

--- src/handlers/getNetworthOverview.ts
+++ src/handlers/getNetworthOverview.ts
@@ -1,5 +1,22 @@
-import getNetWorthVelocity from '../lib/getNetWorthVelocity'
+import * as ynab from 'ynab'
 
-export default function getNetWorhtOverview(db) {
-  return getNetWorthVelocity(db)
+import getNetWorthVelocity from '../lib/getNetWorthVelocity'
+import getTransactions from '../lib/getTransactions'
+import getBudgetId from '../lib/getBudgetId';
+
+export default async function getNetWorhtOverview(db) {
+  const velocity = await getNetWorthVelocity(db)
+  const budgetID = await getBudgetId(process.env.budgetName)
+  const transactions = await getTransactions(budgetID)
+
+  let netWorth = 0
+
+  transactions.forEach(t => {
+    netWorth += ynab.utils.convertMilliUnitsToCurrencyAmount(t.amount, 2)
+  })
+
+  return {
+    velocity,
+    netWorth
+  }
 }
