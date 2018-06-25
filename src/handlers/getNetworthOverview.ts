8ca3791fdd6c3e671dc610b935f029a0eb6b901d
Bastian Gruber <gruberbastian@me.com>
2018-06-25 17:14:26 +0200

    Add networth endpont

--- src/handlers/getNetworthOverview.ts
+++ src/handlers/getNetworthOverview.ts
@@ -1,5 +1,5 @@
 import getNetWorthVelocity from '../lib/getNetWorthVelocity'
 
-export default async function getNetWorhtOverview(db) {
-  return await getNetWorthVelocity(db)
+export default function getNetWorhtOverview(db) {
+  return getNetWorthVelocity(db)
 }
